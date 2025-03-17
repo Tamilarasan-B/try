c2b6d9a11f5aacea8fe1e51a2aafe458e869e39e
Tamilarasan-B <personalmailid57@gmail.com>
2025-03-18 02:53:15 +0530

    Create ex3.java

---
+++ ex3.java
@@ -0,0 +1,14 @@
+public class ex3
+  {
+    public static void main(String[]args)
+    {
+      int a=10;
+      for(int i=1;i<=10;i++)
+        {
+          a=a+10;
+        }
+      System.out.println(a);
+    }
+  }
+    }
+  }
